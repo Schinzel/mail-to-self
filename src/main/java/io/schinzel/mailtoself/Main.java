cdaa98bd7b791ab453b92d562652ddd17726a3a9
Schinzel <henrik@schinzel.se>
2017-08-11 20:55:16 +0200

    Webserver and html page added

--- src/main/java/io/schinzel/mailtoself/Main.java
+++ src/main/java/io/schinzel/mailtoself/Main.java
@@ -1,6 +1,10 @@
 package io.schinzel.mailtoself;
 
 import com.atexpose.AtExpose;
+import com.atexpose.dispatcher.logging.Logger;
+import com.atexpose.dispatcher.logging.LoggerType;
+import com.atexpose.dispatcher.logging.format.LogFormatterFactory;
+import com.atexpose.dispatcher.logging.writer.LogWriterFactory;
 
 public class Main {
 
@@ -11,11 +15,23 @@ public class Main {
         System.out.println("****       -= Mail To Self =-      ****");
         System.out.println("****                               ****");
         System.out.println("***************************************");
-
         AtExpose.create()
                 .startCLI()
                 .getWebServerBuilder()
                 .port(5555)
-                .startWebServer();
+                .accessLevel(1)
+                .cacheFilesInRAM(false)
+                .webServerDir("website")
+                .startWebServer()
+                .addLogger(getEventLogger());
+    }
+
+
+    private static Logger getEventLogger() {
+        return Logger.builder()
+                .loggerType(LoggerType.EVENT)
+                .logFormatter(LogFormatterFactory.JSON.getInstance())
+                .logWriter(LogWriterFactory.SYSTEM_OUT.getInstance())
+                .build();
     }
 }
