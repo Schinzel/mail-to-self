3804dd17e40d2917ab71ba31ccea18ef8c6f9885
Henrik Schinzel <henrik@schinzel.io>
2017-10-07 13:30:40 +0200

    Better notifications

---
+++ src/main/resources/website/mailtoself/shared.js
@@ -0,0 +1,16 @@
+function notify(message) {
+    var notification = document.getElementById("notification");
+    if (!notification) {
+        notification = document.createElement('div')
+        notification.id = 'notification';
+        notification.classList.add('notification');
+        document.body.appendChild(notification);
+    }
+    notification.style.display = 'block';
+    notification.innerHTML = message;
+    setTimeout(function () {
+            document.getElementById("notification").style.display = 'none';
+        },
+        1000);
+}
+
