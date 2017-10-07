75d10745023a8ab22b0f53c4275e2fe75efd2a43
Henrik Schinzel <henrik@schinzel.io>
2017-10-07 14:29:49 +0200

    Clean up server side code

---
+++ src/main/java/io/schinzel/mailtoself/API.java
@@ -0,0 +1,56 @@
+package io.schinzel.mailtoself;
+
+import com.atexpose.Expose;
+import com.atexpose.util.mail.GmailEmailSender;
+import io.schinzel.basicutils.RandomUtil;
+import io.schinzel.basicutils.configvar.ConfigVar;
+import io.schinzel.basicutils.crypto.CipherLibrary;
+import io.schinzel.basicutils.crypto.cipher.Aes256Gcm;
+
+/**
+ * The purpose of this class is to hold the API methods.
+ */
+public class API {
+    private static final int SUBJECT_MAX_LENGTH = 20;
+    private CipherLibrary mCipherLibrary;
+
+
+    API() {
+        //Create the crypto used to encrypt the password stored on the client
+        String cryptoKeyEmailV1 = ConfigVar.create(".env").getValue("CRYPTO_KEY_EMAIL_V1");
+        //Add the email crypto to the cipher library
+        mCipherLibrary = CipherLibrary.create().addCipher(1, new Aes256Gcm(cryptoKeyEmailV1));
+        //Do a warm up encryption. On a 2017 MaxBook pro the first encryption to approximately
+        //200ms and the the second encryption 0.2ms.
+        mCipherLibrary.encrypt(1, RandomUtil.getRandomString(20));
+    }
+
+
+    @Expose(
+            requiredArgumentCount = 3,
+            requiredAccessLevel = 1,
+            arguments = {"Message", "Username", "Password"},
+            description = "Sends an email")
+    String mailMe(String message, String username, String password) {
+        String subject = message.length() > SUBJECT_MAX_LENGTH
+                ? message.substring(0, SUBJECT_MAX_LENGTH) + "..."
+                : message;
+        String clearTextPassword = mCipherLibrary.decrypt(password);
+        Runnable r = () -> {
+            new GmailEmailSender(username, clearTextPassword).send(username, subject, message, "Me");
+        };
+        new Thread(r, "email-send-thread").start();
+        return "Mail sent: '" + message + "'";
+    }
+
+
+    @Expose(
+            requiredArgumentCount = 1,
+            requiredAccessLevel = 1,
+            arguments = {"Password"},
+            description = "Encrypts the argument password",
+            theReturn = "The argument password encrypted")
+    String encryptPassword(String password) {
+        return mCipherLibrary.encrypt(1, password);
+    }
+}
--- src/main/java/io/schinzel/mailtoself/Main.java
+++ src/main/java/io/schinzel/mailtoself/Main.java
@@ -7,34 +7,60 @@ import com.atexpose.dispatcher.logging.format.LogFormatterFactory;
 import com.atexpose.dispatcher.logging.writer.LogWriterFactory;
 import io.schinzel.basicutils.configvar.ConfigVar;
 
+/**
+ * The purpose of this class is to start mail-to-self application
+ */
 public class Main {
 
     public static void main(String[] args) {
-        //Render start message
-        System.out.println("***************************************");
-        System.out.println("****                               ****");
-        System.out.println("****       -= Mail To Self =-      ****");
-        System.out.println("****                               ****");
-        System.out.println("***************************************");
-        AtExpose atExpose = AtExpose.create();
-        atExpose.getAPI()
-                .expose(new Sender());
-        boolean isProduction = ConfigVar.create(".env")
-                .getValue("ENVIRONMENT")
-                .equalsIgnoreCase("production");
-        int port = Integer.valueOf(ConfigVar.create(".env")
-                .getValue("PORT"));
-        atExpose.startCLI()
+        renderStartUpMessage();
+        AtExpose.create()
+                .expose(new API())
+                .startCLI()
                 .getWebServerBuilder()
-                .port(port)
+                .port(getPort())
                 .accessLevel(1)
-                .cacheFilesInRAM(isProduction)
+                .cacheFilesInRAM(isProduction())
                 .webServerDir("website/mailtoself")
                 .startWebServer()
                 .addLogger(getEventLogger());
     }
 
 
+    /**
+     * Render a system start message.
+     */
+    private static void renderStartUpMessage() {
+        System.out.println("***************************************");
+        System.out.println("****                               ****");
+        System.out.println("****       -= Mail To Self =-      ****");
+        System.out.println("****                               ****");
+        System.out.println("***************************************");
+    }
+
+
+    /**
+     * @return The port the web server should use.
+     */
+    private static int getPort() {
+        return Integer.valueOf(ConfigVar.create(".env")
+                .getValue("PORT"));
+    }
+
+
+    /**
+     * @return True if current environment is production. Else false.
+     */
+    private static boolean isProduction() {
+        return ConfigVar.create(".env")
+                .getValue("ENVIRONMENT")
+                .equalsIgnoreCase("production");
+    }
+
+
+    /**
+     * @return The event logger to use to log requests to web server.
+     */
     private static Logger getEventLogger() {
         return Logger.builder()
                 .loggerType(LoggerType.EVENT)
@@ -42,4 +68,5 @@ public class Main {
                 .logWriter(LogWriterFactory.SYSTEM_OUT.create())
                 .build();
     }
+
 }
