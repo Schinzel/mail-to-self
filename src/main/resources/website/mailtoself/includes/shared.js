219bef190d3c946f5fd136e53b82259d5b5f6322
Henrik Schinzel <henrik@schinzel.io>
2018-03-17 11:27:27 +0100

    Comments

--- src/main/resources/website/mailtoself/includes/shared.js
+++ src/main/resources/website/mailtoself/includes/shared.js
@@ -1,13 +1,23 @@
+/**
+ * Display the argument message to user
+ * @param message Message to display
+ */
 function notify(message) {
+    //Get element to place notification in
     var notification = document.getElementById("notification");
+    //If there was no notification element on page
     if (!notification) {
+        //Add notification element
         notification = document.createElement('div');
         notification.id = 'notification';
         notification.classList.add('notification');
         document.body.appendChild(notification);
     }
-    notification.style.display = 'block';
+    //Set notification text
     notification.innerHTML = message;
+    //Display the notification
+    notification.style.display = 'block';
+    //Make the notification invisible after little bit
     setTimeout(function () {
             document.getElementById("notification").style.display = 'none';
         },
